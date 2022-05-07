68238a14d47d04c31b2358c7d72bba0b79fa95b4
Ibrar Ahmed <50750853+ibrarahmedsoomro@users.noreply.github.com>
2022-05-06 22:00:59 -0700

    created  a routers and controllers for memories project

--- src/app.js
+++ src/app.js
@@ -2,23 +2,26 @@ import express from "express";
 import bodyParser from 'body-parser'
 import mongoose from "mongoose";
 import cors from 'cors'
+import postRouter from './routes/posts.js'
+
 const port = process.env.PORT || 5000
+// require('./Db/Connection')
+
 
 
 const app = express()
-
-
 app.use(bodyParser.json({limit : "30mb" , extended : true}))
 app.use(bodyParser.urlencoded({limit : "30mb" , extended : true}))
-
 app.use(cors())
+app.use('/posts', postRouter)
+const Db =  'mongodb+srv://ibrarsoomro:Soomro12345@cluster0.l4age.mongodb.net/socialmemories?retryWrites=true&w=majority'
+mongoose.connect(Db)
+.then( ()=> console.log('Server is Conencted'))
+.catch( (err)=> console.log(err, "Server issue"))
 
-app.get( (res, req)=>{
-    
+app.get("/",  (res, req)=>{
+    res.setEncoding("Server halae tho")
 })
 
-mongoose.connect({useNewUrlParser : true, useUnifiedTopology : true})
-.then( ()=> app.listen(port, ()=> console.log(`Server is running ${port}`))
-.catch( (err) => console.log(err, "Connection problem")))
 
-mongoose.set('useFindAndModify',  false)
+app.listen(port , ()=>console.log(`Server is running http://localhost:${port}`))
---
+++ src/controllers/posts.js
@@ -0,0 +1,8 @@
+export const getPost=(req,res)=>{
+    res.send("This is Working")
+}
+
+
+export const createPost=(res, req)=>{
+    res.send("Created a Post")
+}
